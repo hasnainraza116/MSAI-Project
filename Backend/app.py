fd8dac96f826998b1fbfc8d61791b426a7d20a9d
hasnain raza <hasnainraza116@gmail.com>
2026-01-15 10:40:57 +0100

    After fixing accuracy metrics

--- Backend/app.py
+++ Backend/app.py
@@ -6,7 +6,7 @@ import pickle
 import pandas as pd
 
 # import the ml model
-with open('model.pkl', 'rb') as f:
+with open('/Users/hasnain/Desktop/MSAI Project/ML_project2/Backend/model.pkl', 'rb') as f:
     model = pickle.load(f)
 
 app = FastAPI()
